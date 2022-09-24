78da381357228dd0b7df8d755fdf4d1cb45836a4
IdlCM4 <irinadelacorte@gmail.com>
2022-09-24 04:16:59 +0200

    Añadidos desde countSheeps hasta reversedStrings

--- Kata.java
+++ Kata.java
@@ -256,7 +256,333 @@ public class Kata{
     return r;
   }
 
+  /**
+   * Consider an array/list of sheep where some sheep may be missing from their place. We need a function 
+   * that counts the number of sheep present in the array (true means present).
+   * For example,
+   *    [true,  true,  true,  false,
+   *     true,  true,  true,  true ,
+   *     true,  false, true,  false,
+   *     true,  false, false, true ,
+   *     true,  true,  true,  true ,
+   *     false, false, true,  true]
+   * The correct answer would be 17.
+   * Hint: Don't forget to check for bad values like null/undefined
+   * @param arrayOfSheeps
+   * @return
+   */
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    int sum = 0;
+    for(int i = 0; i < arrayOfSheeps.length; i++){
+      if(arrayOfSheeps[i] != null && arrayOfSheeps[i] != false){
+        sum++;
+      }
+    }
+    return sum;
+  }
 
+  /**
+   * Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and 
+   * be case insensitive. The string can contain any char.Examples input/output:
+   *    XO("ooxx") => true
+   *    XO("xooxx") => false
+   *    XO("ooxXm") => true
+   *    XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+   *    XO("zzoo") => false
+   * @param str
+   * @return
+   */
+  public static boolean getXO (String str) {
+    return str.toLowerCase().chars().filter(ch -> ch == 'x').count() == str.toLowerCase().chars().filter(ch -> ch == 'o').count();
+  }
+
+  /**
+   * Code as fast as you can! You need to double the integer and return it.
+   */
+  public static int doubleInteger(int i) {
+    return i*2;
+  }
+
+  /**
+   * We need a function that can transform a number (integer) into a string.
+   * What ways of achieving this do you know?
+   * Examples (input --> output):
+   *    123  --> "123"
+   *    999  --> "999"
+   *    -100 --> "-100"
+   * @param num
+   * @return
+   */
+  public static String numberToString(int num) {
+    return String.valueOf(num);
+  }
+
+  /**
+   * Simple, remove the spaces from the string, then return the resultant string.
+   * @param x
+   * @return
+   */
+  public static String noSpace(final String x) {
+    return x.replaceAll(" ", "");
+  }
+
+  /**
+   * Write a function which converts the input string to uppercase.
+   * @param str
+   * @return
+   */
+  public static String MakeUpperCase(String str){
+    return str.toUpperCase();
+  }
+
+  /**
+   * Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can 
+   * be built with the sides of given length and false in any other case.
+   * (In this case, all triangles must have surface greater than 0 to be accepted).
+   * @param a
+   * @param b
+   * @param c
+   * @return
+   */
+  public static boolean isTriangle(int a, int b, int c){
+    return (a+b>c && a+c>b && b+c>a) ? true : false;
+  }
+
+  /**
+   * An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that 
+   * determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. 
+   * Ignore letter case.
+   * Example (Input --> Output):
+   *    "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+   * @param str
+   * @return
+   */
+  public static boolean  isIsogram(String str) {
+    str = str.toLowerCase();
+    return (str.chars().distinct().count() == str.length());
+
+  } 
+
+  /**
+   * Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+   * We need a function that can transform a string into a number. What ways of achieving this do you know?
+   * Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+   * Examples:
+   *    "1234" --> 1234
+   *    "605"  --> 605
+   *    "1405" --> 1405
+   *    "-7" --> -7
+   * @param str
+   * @return
+   */
+  public static int stringToNumber(String str) {
+    return Integer.valueOf(str);
+  }
+
+  /**
+   * This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+   * @param n
+   * @return
+   */
+  public static int simpleMultiplication(int n) {
+    return n%2 != 0 ? n*9 : n*8;
+  }
+
+  /**
+   * Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. 
+   * Return the resulting string.
+   * Note: input will never be an empty string
+   * @param numberString
+   * @return
+   */
+  public static String fakeBin(String numberString) {
+    return numberString.replaceAll("[0-4]", "0").replaceAll("[5-9]", "1");
+  }
+
+  /**
+   * Your task is to create a function that does four basic mathematical operations.
+   * The function should take three arguments - operation(string/char), value1(number), value2(number).
+   * The function should return result of numbers after applying the chosen operation.
+   * Examples (Operator, value1, value2) --> output :
+   *    ('+', 4, 7) --> 11
+   *    ('-', 15, 18) --> -3
+   *    ('*', 5, 5) --> 25
+   *    ('/', 49, 7) --> 7
+   * @param op
+   * @param v1
+   * @param v2
+   * @return
+   */
+  public static Integer basicMath(String op, int v1, int v2){
+    switch(op){
+      case "+": return v1 + v2;
+      case "-": return v1 - v2;
+      case "*": return v1 * v2;
+      case "/": return v1 / v2;
+      default: return 0;
+    }
+  }
+
+  /**
+   * Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different 
+   * items in the array that, when added together, give the target value. The indices of these items should then be returned in 
+   * a tuple / list (depending on your language) like so: (index1, index2).
+   * For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+   * The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target 
+   * will always be the sum of two different items from that array).
+   * Based on: http://oj.leetcode.com/problems/two-sum/
+   * @param numbers
+   * @param target
+   * @return
+   */
+  public static int[] twoSum(int[] numbers, int target) {
+    boolean result = false;
+    int k = 0;
+    int[] sol = new int[2];
+    while(!result && k < numbers.length){
+      int attempt = numbers[k];
+      sol[0] = k;
+      for(int i = k + 1; i < numbers.length; i++){
+        if((attempt + numbers[i]) == target){
+          sol[1] = i;
+          result = true;
+        }
+      }
+      k++;
+    }
+    return sol;
+  }
+
+  /**
+   * Create a function which answers the question "Are you playing banjo?".
+   * If your name starts with the letter "R" or lower case "r", you are playing banjo!
+   * The function takes a name as its only argument, and returns one of the following strings:
+   *    name + " plays banjo" 
+   *    name + " does not play banjo"
+   * Names given are always valid strings.
+   * @param name
+   * @return
+   */
+  public static String areYouPlayingBanjo(String name) {
+    if(name.charAt(0) == 'R' || name.charAt(0) == 'r') return name + " plays banjo";
+    else return name + " does not play banjo";
+  }
+
+  /**
+   * Given a list of integers, determine whether the sum of its elements is odd or even.
+   * Give your answer as a string matching "odd" or "even".
+   * If the input array is empty consider it as: [0] (array with a zero).
+   * Examples:
+   *    Input: [0]
+   *    Output: "even"
+   * 
+   *    Input: [0, 1, 4]
+   *    Output: "odd"
+   *    
+   *    Input: [0, -1, -5]
+   *    Output: "even"
+   * @param array
+   * @return
+   */
+  public static String oddOrEven (int[] array) {
+    int number = 0;
+    for(int i = 0; i < array.length; i++){
+        number += array[i];
+    }
+    return (number%2==0)?"even":"odd";
+  }
+
+  /**
+   * Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly 
+   * n times.
+   * Examples (input -> output): 
+   *    6, "I"     -> "IIIIII"
+   *    5, "Hello" -> "HelloHelloHelloHelloHello"
+   * @param repeat
+   * @param string
+   * @return
+   */
+  public static String repeatStr(final int repeat, final String string) {
+    String result = "";
+    for(int i = 0; i < repeat; i++){
+      result += string;
+    }  
+    return result;
+  }
+
+  /**
+   * Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+   * Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+   * Examples:
+   *    Testing: [0, 0, 0, 1] ==> 1
+   *    Testing: [0, 0, 1, 0] ==> 2
+   *    Testing: [0, 1, 0, 1] ==> 5
+   *    Testing: [1, 0, 0, 1] ==> 9
+   *    Testing: [0, 0, 1, 0] ==> 2
+   *    Testing: [0, 1, 1, 0] ==> 6
+   *    Testing: [1, 1, 1, 1] ==> 15
+   *    Testing: [1, 0, 1, 1] ==> 11
+   * @param binary
+   * @return
+   */
+  public static int convertBinaryArrayToInt(List<Integer> binary) {
+    int result = 0, j = 0;
+    for(int i = binary.size() - 1; i >= 0; i--){
+        result += binary.get(i)*(Math.pow(2, j));
+        j++;
+    }
+    return result;
+  }
+
+  /**
+   * You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return 
+   * the middle character. If the word's length is even, return the middle 2 characters.
+   * #Examples:
+   *    Kata.getMiddle("test") should return "es"
+   *    Kata.getMiddle("testing") should return "t"
+   *    Kata.getMiddle("middle") should return "dd"
+   *    Kata.getMiddle("A") should return "A"
+   * #Input
+   * A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error 
+   * in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your 
+   * solution timing out.
+   * #Output
+   * The middle character(s) of the word represented as a string.
+   * @param word
+   * @return
+   */
+  public static String getMiddle(String word) {
+    if(word.length()%2 == 0){ //It's Even
+      return word.substring(word.length()/2 - 1, word.length()/2 + 1);
+    }
+    else{   //It's odd
+      return (word.substring(word.length()/2, word.length()/2 + 1));
+    }
+  }
+
+  /**
+   * Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+   * @param number
+   * @return
+   */
+  public static String evenOrOdd(int number) {
+    return (number%2!=0) ? "Odd" : "Even";
+  }
+
+  /**
+   * Complete the solution so that it reverses the string passed into it.
+   *    'world'  =>  'dlrow'
+   *    'word'   =>  'drow'
+   * @param str
+   * @return
+   */
+  public static String reversedStrings(String str) {
+    String sol = "";
+    for(int i = str.length()-1; i >= 0; i--){ //go trough the String from end to beginning 
+      sol += str.charAt(i); //save each character on a solution String
+    }
+    return sol;
+  }
 
   /**** TESTS *****/
   public static void main(String[] args) {
